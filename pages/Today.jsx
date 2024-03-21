de8ba210016b86a109db02b5ed4fe1544432f17f
noel-pena <azulu88@hotmail.com>
2024-03-21 01:15:50 -0400

    footer

--- pages/Today.jsx
+++ pages/Today.jsx
@@ -1,11 +1,13 @@
 import { ContainerBox } from "../src/components/ContainerBox";
 import { Title } from "../src/components/Title";
+import { Footer } from "../src/components/Footer";
 
 function Today() {
   return (
     <>
       <Title />
       <ContainerBox />
+      <Footer />
     </>
   );
 }
