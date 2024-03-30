d15125b6fed657fa7b81b1ce780a5cb8a977e5da
noel-pena <azulu88@hotmail.com>
2024-03-30 16:51:05 -0400

    deleted react router

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { Item, connectToMongoDB } from "./mongodb.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Today from "../pages/Today";
-import Week from "../pages/Week";
-import Month from "../pages/Month";
+import { ContainerBox } from "../src/components/ContainerBox";
+import { Title } from "../src/components/Title";
+import { Footer } from "../src/components/Footer";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Today />} />
-          <Route path="/Week" element={<Week />} />
-          <Route path="/Month" element={<Month />} />
-        </Routes>
-      </Router>
+      <Title />
+      <ContainerBox />
+      <Footer />
     </>
   );
 }
