362fb188d9ee2df57f4f3b01dccef11571408014
noel-pena <azulu88@hotmail.com>
2024-04-10 23:16:12 -0400

    new code

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,19 @@
 import "./App.css";
-import { ContainerBox } from "./components/ContainerBox";
-import { Title } from "./components/Title";
-import { Footer } from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import TodayNotes from "./pages/TodayNotes";
+import WeekNotes from "./pages/WeekNotes";
+import MonthNotes from "./pages/MonthNotes";
 
 function App() {
   return (
     <>
-      <Title />
-      <ContainerBox />
-      <Footer />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<TodayNotes />} />
+          <Route path="/week" element={<WeekNotes />} />
+          <Route path="/month" element={<MonthNotes />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ client/pages/MonthNotes.jsx
@@ -0,0 +1,16 @@
+import "../App.css";
+import { ContainerBox } from "../components/ContainerBox";
+import { Title } from "../components/Title";
+import { Footer } from "../components/Footer";
+
+function MonthNotes() {
+  return (
+    <>
+      <Title />
+      <ContainerBox />
+      <Footer />
+    </>
+  );
+}
+
+export default MonthNotes;
